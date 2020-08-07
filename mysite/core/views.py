03c67519d5ee7e00fbff7bed0c92553e9adf6cc2
bbbkbc <blazejstangierski@gmail.com>
2020-08-07 15:07:03 +0200

    Initial commit

---
+++ mysite/core/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
+
+def home(request):
+    count = User.objects.count()
+    return render(request, 'home.html', {'count': count})
+
+
+def signup(request):
+    # reg form, if posted and valid then go to other page
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+@login_required
+def secret_page(request):
+    return render(request, 'secret_page.html')
+
+
+class SecretPage(TemplateView):
+    template_name = 'secret_page.html'
---
+++ mysite/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from mysite.core import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('secret/', views.secret_page, name='secret'),
+    path('secret_2/', views.SecretPage.as_view(), name='secret_2'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+]
